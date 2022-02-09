eb3a4cfd6ffc4211223588a274d55f4a9a565e71
cmrib <ciceromirandar@gmail.com>
2022-02-09 10:46:47 -0300

    remove empty lines

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,7 @@
 import { useEffect, useState } from 'react';
 import { SideBar } from './components/SideBar';
 import { Content } from './components/Content';
-import { MovieCard } from './components/MovieCard';
-import { Button } from './components/Button';
 import { api } from './services/api';
-
 import './styles/global.scss';
 import './styles/sidebar.scss';
 import './styles/content.scss';
